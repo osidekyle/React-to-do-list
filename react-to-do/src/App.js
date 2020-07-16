10eb7a45c9d877e8a06d813a5209b2a1547ddf26
osidekyle <hawkinskyle67@gmail.com>
2020-07-15 18:41:48 -0700

    using state as a prop

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
+import Todos from "./components/Todos";
 
-function App() {
+class App extends Component {
+  state=
+  {
+    todos:[
+      {
+        id:1,
+        title:"Take out the trash",
+        completed:false,
+      }
+      ,
+      {
+        id:2,
+        title:"Workout",
+        completed:false,
+      }
+      ,
+      {
+        id:3,
+        title:"Homework",
+        completed:false,
+      }
+    ]
+  }
+
+
+  render(){
+    console.log(this.state.todos)
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos todos={this.state.todos}/>
     </div>
   );
+}
 }
 
 export default App;
