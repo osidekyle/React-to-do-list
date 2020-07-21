1c289243e70148e40fc3c5b15fc9d4e99e57936c
osidekyle <hawkinskyle67@gmail.com>
2020-07-20 23:13:26 -0700

    uuid for random ids

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -3,25 +3,26 @@ import './App.css';
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header"
 import AddTodo from "./components/AddTodo"
+import uuid from "uuid";
 
 class App extends Component {
   state=
   {
     todos:[
       {
-        id:1,
+        id:uuid.v4(),
         title:"Take out the trash",
         completed:false,
       }
       ,
       {
-        id:2,
+        id:uuid.v4(),
         title:"Workout",
         completed:false,
       }
       ,
       {
-        id:3,
+        id:uuid.v4(),
         title:"Homework",
         completed:false,
       }
@@ -50,7 +51,7 @@ class App extends Component {
   //Add Todo
   addTodo = (title) =>{
     const newTodo={
-      id: 4,
+      id: uuid.v4(),
       title,
       completed:false
     }
@@ -61,7 +62,7 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
       <Header/>
       <AddTodo addTodo={this.addTodo}/>
       <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
